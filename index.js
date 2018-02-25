a81bd22b37bb83249ac65f2af91d62b4e43916f3
Ali Haider <dutchland2013@hotmail.com>
2018-02-25 04:10:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,20 +9,51 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+ var object = {[itemName]: Math.floor(Math.random(1,100) * 100)};
+ cart.push(object);
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
-}
+  if(!cart.length) {
+    console.log('Your shopping cart is empty.');
+  }
+  else {
+    var items = [];
+    for(var i=0; i < cart.length; i++) {
+      for(var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item]);
+      }
+    }
+    console.log(`In your cart, you have ${items.join(" and ") + "."}`);
+  }
+  }
 
 function total() {
-  // write your code here
+  var totalValue = [];
+  for(var i=0; i< cart.length; i++) {
+    for(var item in cart[i]) {
+      totalValue.push(cart[i][item]);
+      var sum = totalValue.reduce(function(a, b) {return a + b;}, 0);
+      console.log(`The total value is ${sum}`);
+    }
+  }
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i=0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item) === true) {
+      console.log(cart[i])
+      delete cart[i][item];
+      console.log(cart);
+    }
+    else {
+      console.log("false");
+    }
+  }
+  return cart;
 }
 
 function placeOrder(cardNumber) {
