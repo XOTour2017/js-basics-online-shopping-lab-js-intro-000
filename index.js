8ed0829c2ccec61b106951e99f776f0769e22a79
Ali Haider <dutchland2013@hotmail.com>
2018-02-25 04:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,20 +16,46 @@ function addToCart(itemName) {
  return cart;
 }
 
-function viewCart() {
+function viewCarted() {
   if(!cart.length) {
     console.log('Your shopping cart is empty.');
   }
   else {
     var items = [];
+    // for loop to iterate over objects in cart
     for(var i=0; i < cart.length; i++) {
+      // another for loop to iterate over the object's keys
       for(var item in cart[i]) {
+        // adding the key and value into the items array
         items.push(item + " at $" + cart[i][item]);
       }
     }
-    console.log(`In your cart, you have ${items.join(" and ") + "."}`);
+    //  checking if items array length is equal to 2
+    // if it is, then it'll add an "and" after the first comma and finish the function
+    // if not then output will be: In your cart, you have banana at $77.
+    if(items.length == 2) {
+      return(`In your cart, you have ${items.join(" and ") + "."}`);
+    }
+    // otherwise if there is three or more items in the cart then these conditions will activate
+    else {
+      // another for loop to iterate over the cart array
+      for(var x=0; x < cart.length; x++) {
+        // testing to see the indexes: 0,1,2
+        console.log(x)
+        // condition to see if it isn't the last item in cart
+        if(x+1 != cart.length) {
+           console.log(`In your cart, you have ${items.join(", ") + "."}`);
+        }
+        // condition if it is the last item
+        else {
+          const mostItems = items.slice(0, -1);
+          const lastItem = items.slice(-1)[ 0 ];
+          console.log(`In your cart, you have ${mostItems.join(", and ")}${lastItem}.`);
+        }
+      }
+    }
   }
-  }
+}
 
 function total() {
   var totalValue = [];
